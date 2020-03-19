5efec50c1fcba3a5f8335090f646530f9719f7a2
shimadu13 <tfsh.tjh.kne@gmail.com>
2020-03-19 17:19:26 +0900

    #2 loading追加

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -4,25 +4,38 @@ import { translateJaToEn } from '../api/translateApi';
 function Translate() {
 
     const [lang, setLang] = React.useState('');
+    // ①:通信中かどうかを管理するStateを定義
+    const [loading, setLoading] = React.useState(false);
 
     const input = React.useRef(null);
 
     const onClick = async () => {
         try {
+            // ②:通信処理実行前にLoadingにtrueをset
+            setLoading(true);
             const response = await translateJaToEn(input.current.value);
             setLang(response.data);
         } catch (e) {
             alert(e.toString());
+        } finally {
+            // ③:処理が完了したloadingにfalseをset
+            setLoading(false);
         }
     };
 
     return (
-
         <div>
-            <input ref={input} />
-            <button onClick={onClick}>日⇒英</button>
-            <p>{lang}</p>
-        </div>
+            {loading ? (
+                < p > Loading...</p>
+            ) : (
+                    <div>
+                        <input ref={input} />
+                        <button onClick={onClick}>日⇒英</button>
+                        <p>{lang}</p>
+                    </div>
+                )
+            }
+        </div >
     );
 }
 
